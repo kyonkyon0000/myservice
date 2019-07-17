b68157659aa1cf013a201e4052e560923e359555
kyonkyon <turna.0000@gmail.com>
2019-07-17 17:54:55 +0900

    投稿関係の機能を実装完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,59 @@
 class PostsController < ApplicationController
-
-  def index #投稿一覧
+  #投稿一覧
+  def index
     @posts = Post.all.order(created_at: :desc)
   end
 
-  def show #投稿詳細表示
+  #投稿詳細表示
+  def show
     @post = Post.find_by(id: params[:id])
   end
 
-  def new #新規投稿画面
+  #新規投稿画面
+  def new
     @post = Post.new #空のインスタンスを作成
   end
 
-  def create #投稿ボタン押下時
+  #投稿ボタン押下時
+  def create
     #インスタンスを作成(contentフォームから送られてきたデータをcontentカラムに入れる)
     @post = Post.new(content: params[:content]) # "content" => params[:content]
     if @post.save #DBに保存できたら
       flash[:notice] = "投稿しました" #フラッシュを作成
       redirect_to("/posts/index") #上記動作後に投稿一覧画面に遷移
     else
-      render("posts/new")
+      #newアクションを飛ばしてnew.html.erbに遷移
+      render("posts/new") #new.htmlでcreateアクションの@postが使えるようになる
     end
+  end
 
+  #編集画面
+  def edit
+    #データベースから該当するidのデータを取ってくる
+    @post = Post.find_by(id: params[:id])
+  end
+
+  #編集ボタン押下時
+  def update
+    #データベースから該当するidのデータを取ってくる
+    @post = Post.find_by(id: params[:id])
+    #撮ってきたデータに編集データを入れる
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      #editアクションを飛ばしてedit.html.erbに遷移
+      render("posts/edit") #edit.htmlでupdateアクションの@postが使えるようになる
+    end
+  end
+
+  #削除ボタン押下時
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get "posts/index" => "posts#index" #投稿一覧
   get "posts/new" => "posts#new" #新規投稿画面
   post "posts/create" => "posts#create" #投稿ボタン押下時
+  get "posts/:id/edit" => "posts#edit" #編集画面
+  post "posts/:id/update" => "posts#update" #編集ボタン押下時
+  post "posts/:id/destroy" => "posts#destroy" #削除ボタン押下時
+
   get "posts/:id" => "posts#show" #投稿詳細
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
